4281b99d01372d4235438fda4666d687e17ad97d
tinhonline262 <tinhonline262@gmail.com>
2024-10-23 20:09:19 +0700

    Add new solution project

---
+++ MentorBookingSystemAPI/MentorBooking.Repository/Models/PointTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MentorBooking.Repository.Models;
+
+public partial class PointTransaction
+{
+    public Guid TransactionId { get; set; }
+
+    public int? PointsChanged { get; set; }
+
+    public bool TransactionType { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? CreateAt { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public virtual UserPoint User { get; set; } = null!;
+}
