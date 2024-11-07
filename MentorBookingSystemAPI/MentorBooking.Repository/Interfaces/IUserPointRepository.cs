9a275828c1f6b124ef95b1c182084ad07b6231ed
NguyenAn3001 <nguyenlinh357361@gmail.com>
2024-11-07 22:22:49 +0700

    nothing

--- MentorBookingSystemAPI/MentorBooking.Repository/Interfaces/IUserPointRepository.cs
+++ MentorBookingSystemAPI/MentorBooking.Repository/Interfaces/IUserPointRepository.cs
@@ -6,4 +6,5 @@ public interface IUserPointRepository
 {
     Task<bool> SetUserPoint(Guid userId, int point, string descriptionTransaction = "Initial point.");
     Task<UserPoint> GetUserPoint(Guid userId);
+    Task SetUserPoint(Guid studentId, int? pointPayment);
 }
