71b73b6fb284081effcd714d03f40d3e53ffc6d4
Col2609 <181680301+Col2609@users.noreply.github.com>
2024-11-16 21:07:17 +0700

    .

---
+++ MentorBookingSystemAPI/MentorBooking.Repository/Migrations/20241116135638_AddBlogCreatedTimestamp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MentorBooking.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlogCreatedTimestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
